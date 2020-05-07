5a52bd4ae3b089fc9d02ee2f5837cf9ab32d74b0
karry26 <47284842+karry26@users.noreply.github.com>
2020-05-07 21:43:13 +0530

    *

--- server.js
+++ server.js
@@ -9,18 +9,14 @@ app.listen(8080,function(){
 
 });
 //console.log(__dirname+"/uploads")
-app.use('/uploads',express.static(path.join(__dirname,"uploads")))
+app.use('/public',express.static(path.join(__dirname,"public")))
 app.get("/",function(req,resp)
 {
     resp.setHeader("content-type","text/html");
-    var filePath=path.join(__dirname,"index.html");
+    var filePath=path.join(__dirname,"public","index.html");
     resp.sendFile(filePath);
     //resp.sendFile("index.html");
    // resp.end()
 })
 var scrapper=require("./cheerio.js")
 app.use("/cheerio.js",scrapper)
-app.get("/cheerio.js",function(req,resp){
-    console.log(req.query)
-    //resp.sendFile(path.join(__dirname,"cheerio.js"))
-})
